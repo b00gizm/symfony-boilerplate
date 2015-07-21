cc7c03c2d35c0ec63423f8b379c767943f6728d2
Pascal Cremer <b00gizm@gmail.com>
2015-07-21 15:42:02 +0200

    Added Elastica and made it available as container services
    You can access
    - Elastica\Client under "eleastic.client" key
    - Elastica\Search under "eleastic.search" key

--- myapp/app/config/parameters.php
+++ myapp/app/config/parameters.php
@@ -8,3 +8,6 @@ $dotenv->load();
 $container->setParameter('database_host', getenv('MYSQL_PORT_3306_TCP_ADDR'));
 $container->setParameter('database_port', getenv('MYSQL_PORT_3306_TCP_PORT'));
 $container->setParameter('database_password', getenv('MYSQL_ROOT_PASSWORD'));
+
+$container->setParameter('elasticsearch_host', getenv('ELASTICSEARCH_PORT_9200_TCP_ADDR'));
+$container->setParameter('elasticsearch_port', getenv('ELASTICSEARCH_PORT_9200_TCP_PORT'));
